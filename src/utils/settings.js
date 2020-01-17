356fbb51afa6f92f0e83f236f689682a98cea424
dingxl <dingxl>
2020-01-17 11:57:51 +0800

    set docker url

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,6 +1,7 @@
 // use localStorage to store the settings
 
-const API_URL = 'http://127.0.0.1:8011/';
+const API_URL = 'http://api:8011/';
+// const API_URL = 'http://127.0.0.1:8011/';
 // const API_URL = 'http://192.168.1.157:8011/';
 
 export function getApiUrl() {
