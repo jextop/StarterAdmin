b0fc6f9bf0e53ccd7e438ed73a458cae7967c04a
dingxl <dingxl>
2020-02-04 09:38:01 +0800

    Build image from admin_base

---
+++ pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker pull mysql:5
+docker pull adminer:latest
+docker pull redis:4
+docker pull webcenter/activemq:latest
+docker pull elasticsearch:latest
+docker pull registry.cn-shanghai.aliyuncs.com/hellodock/logstash:7.5.2
+docker pull registry.cn-shanghai.aliyuncs.com/jext/starter_api:latest
+docker pull registry.cn-shanghai.aliyuncs.com/jext/starter_admin_base:latest
