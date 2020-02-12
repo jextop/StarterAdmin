ae4105c4d58839a75ecac7cfd8cca8a2e5e73ab1
dingxl <dingxl>
2020-02-12 15:53:40 +0800

    #2 列表方式显示接口chk返回的子服务信息

--- src/pages/setting/chk/index.jsx
+++ src/pages/setting/chk/index.jsx
@@ -1,13 +1,12 @@
-import React, { Component, Suspense } from 'react';
-import { GridContent } from '@ant-design/pro-layout';
+import { Button, Card, Icon, List, Typography } from 'antd';
+import React, { Component } from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { connect } from 'dva';
-import PageLoading from './components/PageLoading';
+import styles from './style.less';
 
-const IntroduceRow = React.lazy(() => import('./components/IntroduceRow'));
+const { Paragraph } = Typography;
 
 class Check extends Component {
-  state = {};
-
   timer = undefined;
   reqRef = undefined;
 
@@ -38,19 +37,69 @@ class Check extends Component {
   render() {
     const { setting, loading } = this.props;
 
+    function formatArr(objArr) {
+      let arr = []
+      for (const i in objArr) {
+        const obj = objArr[i]
+        if (obj !== undefined) {
+          arr.push(obj)
+        }
+      }
+
+      const objStr = JSON.stringify(arr, null, 1);
+      if (objStr === undefined) {
+        return undefined;
+      }
+      return objStr.substring(1, objStr.length - 1);
+    };
+
     return (
-      <GridContent>
-        <React.Fragment>
-          <Suspense fallback={<PageLoading />}>
-            <IntroduceRow loading={loading} visitData={setting} />
-          </Suspense>
-        </React.Fragment>
-      </GridContent>
+      <PageHeaderWrapper >
+        <div className={styles.cardList}>
+          <List
+            rowKey="chk"
+            loading={loading}
+            grid={{
+              gutter: 24,
+              lg: 3,
+              md: 2,
+              sm: 1,
+              xs: 1,
+            }}
+            dataSource={[setting, ...setting.services]}
+            renderItem={item => {
+              return (
+                <List.Item key={item.chk}>
+                  <Card
+                    hoverable
+                    className={styles.card}
+                  >
+                    <Card.Meta
+                      avatar={item.chk}
+                      title={formatArr([item.msg])}
+                      description={
+                        <Paragraph
+                          className={styles.item}
+                          ellipsis={{
+                            rows: 3,
+                          }}
+                        >
+                          {formatArr([item.date, item.status, item.count])}
+                        </Paragraph>
+                      }
+                    />
+                  </Card>
+                </List.Item>
+              );
+            }}
+          />
+        </div>
+      </PageHeaderWrapper>
     );
   }
 }
 
 export default connect(({ setting, loading }) => ({
   setting,
-  loading: loading.effects['setting/chk'],
+  loading: loading.models.setting,
 }))(Check);
